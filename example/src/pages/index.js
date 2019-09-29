367e0a18f6259957aaded03467f0c36eabffb521
Hunter Chang <hunter@madwiremedia.com>
2019-09-29 11:37:49 -0600

    emotion to example

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-export default () => <div className="container mx-auto">Homepage in a user's site</div>;
+const Description = styled.div`
+  padding: 20px;
+  background: #000;
+  color: #fff;
+`;
+
+export default () => (
+  <div className="container mx-auto">
+    <p>Homepage in a user's site</p>
+    <p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </p>
+    <Description>Emotion</Description>
+  </div>
+);
