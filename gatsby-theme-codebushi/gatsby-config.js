c7254697e6f104d2ec48586bab2b22121e2fd6c5
Hunter Chang <hunter@madwiremedia.com>
2019-09-29 11:30:36 -0600

    adding tailwind and purge css

--- gatsby-theme-codebushi/gatsby-config.js
+++ gatsby-theme-codebushi/gatsby-config.js
@@ -1,19 +1,30 @@
-module.exports = {
+const tailwindcss = require("tailwindcss");
+const path = require("path");
+
+module.exports = ({
+  tailwind: { configPath = `${__dirname}/tailwind.config.js` } = {},
+  purgeCSS: { purgeOnly = [path.join(__dirname, "src/css/tailwind.css")], content = [] } = {}
+}) => ({
   plugins: [
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `content/posts`,
-        name: `posts`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`]
-      }
-    },
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [tailwindcss(configPath), require("autoprefixer")]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly,
+        content: [
+          path.join(__dirname, "src/**/!(*.d).{ts,js,jsx,tsx}"),
+          path.join(process.cwd(), "src/**/!(*.d).{ts,js,jsx,tsx}"),
+          ...content
+        ]
+      }
+    }
   ]
-};
+});
