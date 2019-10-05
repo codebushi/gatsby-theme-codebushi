db215dfbb80e1f449338106a7edd0e9d15fa0c8c
Hunter Chang <hunter@madwiremedia.com>
2019-10-05 16:01:04 -0600

    clean index

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,14 +1,7 @@
-import styled from '@emotion/styled';
 import React from 'react';
-import tw from 'tailwind.macro';
-
-const Button = styled.button`
-  ${tw`bg-blue-500 hover:bg-blue-700 text-white p-2 rounded`};
-`;
 
 export default () => (
   <div className="container mx-auto p-8">
     <p>Homepage</p>
-    <Button>hello</Button>
   </div>
 );
