392b293e34857048782d8e15d0e2e1274dd0839b
Hunter Chang <hunter@madwiremedia.com>
2019-10-05 16:54:46 -0600

    clean index

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -3,5 +3,13 @@ import React from 'react';
 export default () => (
   <div className="container mx-auto p-8">
     <p>Homepage</p>
+    <p>
+      <button
+        type="button"
+        className="bg-green-500 hover:bg-green-700 text-white rounded py-2 px-4"
+      >
+        World
+      </button>
+    </p>
   </div>
 );
