3c370a8b20b17bd75441a6eb36086f06c1b56772
Hunter Chang <hunter@madwiremedia.com>
2019-11-10 14:40:41 -0700

    adding themes

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -2,6 +2,95 @@ import React from 'react';
 
 export default () => (
   <div className="container mx-auto p-8">
-    <p>Homepage</p>
+    <div className="theme-startup">
+      <div className="bg-inverse font-body">
+        <div className="container mx-auto px-4">
+          <div className="py-16 text-center">
+            <div className="mb-6">
+              <h1 className="font-display text-inverse font-light text-5xl mb-4">
+                Our Big Awesome Headline
+              </h1>
+              <p className="font-normal text-inverse-soft text-2xl">
+                The subheadline that comes after the headline.
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-primary text-primary px-6 py-3 mr-2"
+              >
+                Main Action
+              </button>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-secondary text-secondary px-6 py-3"
+              >
+                Secondary Action
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="theme-boring">
+      <div className="bg-inverse font-body">
+        <div className="container mx-auto px-4">
+          <div className="py-16 text-center">
+            <div className="mb-6">
+              <h1 className="font-display text-inverse font-light text-5xl mb-4">
+                Our Big Awesome Headline
+              </h1>
+              <p className="font-normal text-inverse-soft text-2xl">
+                The subheadline that comes after the headline.
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-primary text-primary px-6 py-3 mr-2"
+              >
+                Main Action
+              </button>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-secondary text-secondary px-6 py-3"
+              >
+                Secondary Action
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="theme-elegant">
+      <div className="bg-inverse font-body">
+        <div className="container mx-auto px-4">
+          <div className="py-16 text-center">
+            <div className="mb-6">
+              <h1 className="font-display text-inverse font-light text-5xl mb-4">
+                Our Big Awesome Headline
+              </h1>
+              <p className="font-normal text-inverse-soft text-2xl">
+                The subheadline that comes after the headline.
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-primary text-primary px-6 py-3 mr-2"
+              >
+                Main Action
+              </button>
+              <button
+                type="button"
+                className="font-body rounded-btn text-xl font-medium bg-secondary text-secondary px-6 py-3"
+              >
+                Secondary Action
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
