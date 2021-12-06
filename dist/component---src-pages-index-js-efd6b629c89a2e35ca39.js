360ff8715760ca0c20ccf77f875a05686fcb3f92
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-12-06 18:17:32 -0500

    built dist for library testing

---
+++ dist/component---src-pages-index-js-efd6b629c89a2e35ca39.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexample=self.webpackChunkexample||[]).push([[678],{2410:function(e,a,n){n.r(a);n(7378);var t=n(3717);a.default=function(){return(0,t.tZ)("div",{className:"container mx-auto p-8"},(0,t.tZ)("p",null,"Homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-efd6b629c89a2e35ca39.js.map
